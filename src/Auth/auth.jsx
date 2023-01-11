57b63fd2728f5a4b4b9f0f22dfd4a14712add104
DreamLineLove <alixmega11111@gmail.com>
2023-01-11 20:42:28 +0630

    - Changed title font color to black (in landing page) - Replaced the landing page "technologies used" section with more informative "about" and "github" sections

--- src/Auth/auth.jsx
+++ src/Auth/auth.jsx
@@ -244,8 +244,9 @@ export function Auth({ setRenderAs, studentList, setCsPortalUser }) {
             <Text
               h1
               size={68}
+              color="black"
               css={{
-                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                // textGradient: "45deg, $blue600 -20%, $pink600 50%",
                 fontFamily: "serif",
                 fontStyle: "italic",
               }}
@@ -256,8 +257,9 @@ export function Auth({ setRenderAs, studentList, setCsPortalUser }) {
             <Text
               h1
               size={68}
+              color="black"
               css={{
-                textGradient: "45deg, $purple600 -20%, $pink600 100%",
+                // textGradient: "45deg, $purple600 -20%, $pink600 100%",
                 fontFamily: "serif",
                 fontStyle: "italic",
               }}
@@ -268,8 +270,9 @@ export function Auth({ setRenderAs, studentList, setCsPortalUser }) {
             <Text
               h1
               size={68}
+              color="black"
               css={{
-                textGradient: "45deg, $yellow600 -20%, $red600 100%",
+                // textGradient: "45deg, $yellow600 -20%, $red600 100%",
                 fontFamily: "serif",
                 fontStyle: "italic",
               }}
@@ -485,38 +488,76 @@ export function Auth({ setRenderAs, studentList, setCsPortalUser }) {
       <Spacer y={3} />
       <Row>
         <Col>
-          <Text color="#000" size={12}>
-            Created by:
+          <Text color="#000" size={18} css={{ fontFamily: "monospace" }}>
+            developed by:
           </Text>
         </Col>
         <Col>
-          <Text color="#000" size={12}>
-            Zwe Nyan Zaw @1Y1S
+          <Text b color="#000" size={18} css={{ fontFamily: "monospace" }}>
+            Zwe Nyan Zaw
+          </Text>
+          <Text
+            color="#000"
+            size={13}
+            css={{ fontFamily: "monospace", fontStyle: "italic" }}
+          >
+            (2022 matriculated, first year first semester CS @ YU)
           </Text>
         </Col>
       </Row>
       <Spacer y={1} />
-      <Row>
+      <a href="https://www.github.com/DreamLineLove" target="_blank">
+        <Row>
+          <Col>
+            <Text color="#000" size={18} css={{ fontFamily: "monospace" }}>
+              GitHub:{" "}
+            </Text>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>
+          </Col>
+          <Col>
+            <Text b color="#000" size={18} css={{ fontFamily: "monospace" }}>
+              DreamLineLove
+            </Text>
+          </Col>
+        </Row>
+      </a>
+      {/* <Row>
         <Col>
-          <Text color="#000" size={12}>
+          <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
             Tech stack used:
           </Text>
         </Col>
         <Col>
-          <Text color="#000" size={12}>
+          <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
             HTML
           </Text>
-          <Text color="#000" size={12}>
+          <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
             CSS
           </Text>
-          <Text color="#000" size={12}>
+          <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
             Javascript (ReactJS)
           </Text>
-          <Text color="#000" size={12}>
+          <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
             Database (PostgresSQL)
           </Text>
         </Col>
-      </Row>
+      </Row> */}
+      {/* <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
+        Tech stack used: <br />
+        HTML, CSS, Javascript
+      </Text>
+      <Spacer y={1} />
+      <Text color="#000" size={15} css={{ fontFamily: "monospace" }}>
+        javascript librarie: <br />
+        HTML, CSS, Javascript
+      </Text> */}
       <Spacer y={1} />
       <Modal
         blur
